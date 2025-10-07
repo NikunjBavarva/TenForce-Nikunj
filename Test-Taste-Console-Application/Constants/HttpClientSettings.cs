018eeb47b2d8c0cacd0cdf8b56153a9b1ca52b39
Nicky Patel <nikunjp431@gmail.com>
2025-10-07 22:57:58 +0530

    Added API Key

--- Test-Taste-Console-Application/Constants/HttpClientSettings.cs
+++ Test-Taste-Console-Application/Constants/HttpClientSettings.cs
@@ -3,5 +3,6 @@
     public static class HttpClientSettings
     {
         public const string JsonType = "application/json";
+        public const string APIKey = "10e2a4bf-67af-40c2-9a2b-7ac4de350322";
     }
 }
